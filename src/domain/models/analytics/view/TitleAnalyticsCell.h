a1f986f092eb38fbabf9d9b91838c664631f062a
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2024-05-21 01:31:58 +0300

    Закоментировал инструмент отображения графиков

--- src/domain/models/analytics/view/TitleAnalyticsCell.h
+++ src/domain/models/analytics/view/TitleAnalyticsCell.h
@@ -9,11 +9,26 @@
 #include "BaseAnalyticsCell.h"
 #include "src/ui/common/widgets/analytictable/titlecell/TitleCellWidget.h"
 
+/**
+ * Ячейка с заголовком.
+ */
 class TitleAnalyticsCell: public BaseAnalyticsCell {
 
 public:
     QString title;
 
+    /**
+     * Ячейка с заголовком.
+     *
+     * Когда использовать:
+     * Использовать в случае, если нужно отделить одни данные
+     * от других заголовком.
+     *
+     * Пример использования:
+     * new TitleAnalyticsCell("Результаты вычислений графа " + save)
+     *
+     * @param title заголовок, отображающийся крупным шрифтоа.
+     */
     TitleAnalyticsCell(
         QString title
     ) {
