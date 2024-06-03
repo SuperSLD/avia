809a72574ce051d186ced6741de338ebfd7b45c7
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2024-06-03 21:10:51 +0300

    пофиксил работу воркера загрузки сохраненных данных

--- src/domain/usecases/loadsaveddata/LoadSavedDataWorker.cpp
+++ src/domain/usecases/loadsaveddata/LoadSavedDataWorker.cpp
@@ -6,12 +6,13 @@
 
 void LoadSavedDataWorker::run() {
     auto area = Area(settingsRepository->getJson("area"));
-    emit areaLolad(area);
+    if (!area.points.isEmpty()) emit areaLolad(area);
     auto graph = TransportGraphModel(settingsRepository->getJson("airports"));
-    emit airportsLolad(graph, false);
+    if (!graph.airports.isEmpty()) emit airportsLolad(graph, false);
 
     auto saves = QList<QString> {"s1", "s2", "s3", "s4", "s5"};
     foreach(auto s, saves) {
-        emit graphLoad(s, TransportGraphModel(settingsRepository->getJson("airports" + s)));
+        auto savedGraph = TransportGraphModel(settingsRepository->getJson("airports" + s));
+        if (!savedGraph.airports.isEmpty()) emit graphLoad(s, savedGraph);
     }
 }
